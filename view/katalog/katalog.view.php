eb018988df5077c3c24b2ff3f3edfa231c439dd9
gitAmalia24 <nuramaliagita24@gmail.com>
2025-09-09 08:14:28 +0700

    Update katalog view

--- view/katalog/katalog.view.php
+++ view/katalog/katalog.view.php
@@ -3,36 +3,36 @@ class KatalogView extends KatalogController
 {
     public function show()
     {
-        $products = $this->getProducts(); // call func controller product
+        $products = $this->getProducts(); // ambil semua tanaman
 
         foreach ($products as $product) { ?>
             <tr>
-                <td><?php echo $product['id']; ?></td>
-                <td><?php echo $product['nama']; ?></td>
-                <td><?php echo $product['kategori']; ?></td>
-                <td><?php echo $product['manfaat']; ?></td>
+                <td><?= htmlspecialchars($product['id']); ?></td>
+                <td><?= htmlspecialchars($product['nama']); ?></td>
+                <td><?= htmlspecialchars($product['kategori']); ?></td>
+                <td><?= htmlspecialchars($product['manfaat']); ?></td>
             </tr>
-            <?php
+        <?php
         }
     }
 
     public function find()
     {
-        $detailProduct = $this->getKatalogBy(); // call func controller product
+        $detailProduct = $this->getKatalogBy(); // ambil detail tanaman
 
         if (empty($detailProduct)) {
+            echo "<p>Data tanaman tidak ditemukan.</p>";
             return;
         }
 
         foreach ($detailProduct as $detail) { ?>
             <div>
-                <h3><?php echo $detail['name']; ?></h3>
-                <p>kategori: <?php echo $detail['kategori']; ?></p>
-                <p>manfaat: <?php echo $detail['manfaat']; ?></p>
+                <h3><?= htmlspecialchars($detail['nama']); ?></h3>
+                <p>Kategori: <?= htmlspecialchars($detail['kategori']); ?></p>
+                <p>Manfaat: <?= htmlspecialchars($detail['manfaat']); ?></p>
             </div>
-            <?php
+        <?php
         }
     }
-
 }
 ?>
---
+++ view/katalog/utils/navbar.utils.php
@@ -0,0 +1,36 @@
+<ul>
+    <li><a class="active" href="#home">Beranda</a></li>
+    <li><a href="#tanaman">Katalog Tanaman</a></li>
+    <li><a href="#perawatan">Perawatan</a></li>
+    <li><a href="#tentang">Tentang Kami</a></li>
+</ul>
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #3663ea;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+        background-color: #5cceed;
+    }
+
+    .active {
+        background-color: #001ca9;
+    }
+</style>
